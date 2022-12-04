35976391f173e51390347c6907087ea29565455d
Byeongsu KIM <brightdev.bs@gmail.com>
2022-12-04 22:12:00 +0900

    프로젝트 생성

---
+++ src/main/java/vanilla/witchtrial/WitchTrialApplication.java
@@ -0,0 +1,13 @@
+package vanilla.witchtrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitchTrialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WitchTrialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vanilla/witchtrial/WitchTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package vanilla.witchtrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitchTrialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
