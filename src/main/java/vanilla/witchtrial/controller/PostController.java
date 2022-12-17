c4a06e1da0129fa8543710a541512e76e38b396f
Byeongsu KIM <brightdev.bs@gmail.com>
2022-12-18 04:25:55 +0900

    #22 - 게시판 뷰 작성  1. 게시판, 게시글 뷰 작성  2. Spring Security 적용  3. 뷰 테스트 작성

--- src/main/java/vanilla/witchtrial/controller/PostController.java
+++ src/main/java/vanilla/witchtrial/controller/PostController.java
@@ -2,11 +2,29 @@ package vanilla.witchtrial.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
+@RequestMapping("/board")
 @Controller
 public class PostController {
 
+    @GetMapping
+    public String getBoardListView(ModelMap map) {
+        map.addAttribute("boardList", List.of());
+        return "board/index";
+    }
+
+    @GetMapping("/{postId}")
+    public String post(@PathVariable Long postId, ModelMap map) {
+        map.addAttribute("post", "post"); // Todo : 실제 데이터 넣어줘야 함.
+        return "board/postDetail";
+    }
 
 
 }
--- src/test/java/vanilla/witchtrial/controller/PostControllerTest.java
+++ src/test/java/vanilla/witchtrial/controller/PostControllerTest.java
@@ -4,12 +4,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import vanilla.witchtrial.config.SecurityConfig;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(PostController.class)
 class PostControllerTest {
 
@@ -24,7 +28,8 @@ class PostControllerTest {
     void getBoardListView() throws Exception {
         mockMvc.perform(get("/board"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("board/index"))
                 .andExpect(model().attributeExists("boardList"));
     }
 
@@ -33,7 +38,8 @@ class PostControllerTest {
     void getPostDetailView() throws Exception {
         mockMvc.perform(get("/board/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("board/postDetail"))
                 .andExpect(model().attributeExists("post"));
     }
 
