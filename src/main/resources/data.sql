66c5834b250b544a7fad6def7bdde68082ac42c5
Byeongsu KIM <brightdev.bs@gmail.com>
2022-12-20 18:06:49 +0900

    #33 - Auth 기능 추가 스프링 시큐리티 View 적용

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -203,4 +203,4 @@ insert into post_comment (id, post_id, content, created_at, created_by, modified
 insert into post_comment (id, post_id, content, created_at, created_by, modified_at, modified_by) values (100, 100, 'Nulla justo.', '2021-12-22 20:41:35', 'Lida', '2022-03-20 21:04:05', 'Janeva');
 insert into post_comment (id, post_id, content, created_at, created_by, modified_at, modified_by) values (101, 1, 'Nulla justo.', '2021-12-22 20:41:35', 'Lida', '2022-03-20 21:04:05', 'Janeva');
 
-insert into user(id, email, password, username) values(1, 'vanille', 'pw', 'vanille')
+insert into user(id, email, password, username) values(1, 'vanille@gmail.com', '{noop}pw', 'vanille')
